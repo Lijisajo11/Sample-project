afdb89f3cd52f990e30c198b843093766adc860c
lijisajo11 <168018744+Lijisajo11@users.noreply.github.com>
2024-07-08 15:40:31 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-docker login -u lijisajo11 -p dckr_pat_Rzs4YYMcKqUjozrSRrpZz9UTHGM
+docker login -u lijisajo11 -p dckr_pat_JvfWdCvdROgZg3-eH9Ei2mo2f54
 docker tag nginx:latest lijisajo11/dev-repo11
 docker push lijisajo11/dev-repo11
